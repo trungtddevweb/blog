f5987372e9428f40956b9659b9eafd2f45d1d633
Trung <109146090+trungtddevweb@users.noreply.github.com>
2022-09-22 12:21:14 +0700

    SearchCheck

--- src/layouts/components/Search/Search.js
+++ src/layouts/components/Search/Search.js
@@ -8,29 +8,10 @@ import Tippy from '@tippyjs/react/headless';
 import ArticleItem from '~/components/ArticleItem';
 // import axios from 'axios';
 import { useDebounce } from '~/hooks';
+import axios from 'axios';
 const cx = classNames.bind(styles);
 
 const Search = () => {
-    const init = [
-        {
-            userId: 1,
-            id: 1,
-            title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-            body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-        },
-        {
-            userId: 1,
-            id: 2,
-            title: 'qui est esse',
-            body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
-        },
-        {
-            userId: 1,
-            id: 3,
-            title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut',
-            body: 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut',
-        },
-    ];
     // State
     const [searchResult, setSearchResult] = useState([]);
     const [searchValue, setSearchValue] = useState('');
@@ -45,16 +26,13 @@ const Search = () => {
             return;
         }
 
-        // const fetchPost = async () => {
-        //     const result = await axios.get('https://jsonplaceholder.typicode.com/posts/');
-        //     const { data } = result;
-        //     console.log(data);
-        //     setSearchResult(data);
-        // };
-        // fetchPost();
-        setTimeout(() => {
-            setSearchResult(init);
-        }, 1000);
+        const fetchPost = async () => {
+            const result = await axios.get('http://localhost:3001/api/post/');
+            const { data } = result;
+            console.log(data);
+            setSearchResult(data);
+        };
+        fetchPost();
     }, [debounced]);
 
     const handleShowResult = () => {
